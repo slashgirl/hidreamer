ee1a8091b8c50bd5a901986efc4b7f47f4f8650b
slashgirl <403741195@qq.com>
2017-06-21 18:02:46 +0800

    fix some function of http serve
    基于事件驱动的回调；服务器处理请求；行为驱动执行；让请求处理做出响应；

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request,response) {
-	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.write("Hello World");
-	response.end();
-}).listen(8888);
+function start(route,handle){
+	function onRequest(request,response){
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for" + pathname + "received.");
+
+		//响应头部信息
+		//200为状态码，2xx表示请求已成功被服务器接收
+		response.writeHead(200,{"Content-Type":"text/plain"});
+
+		var content = route(handle,pathname)
+
+		response.write(content);
+		response.end();//结束响应
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;
+
