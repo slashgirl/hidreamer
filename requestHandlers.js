cce81c158299353826ac58c5ba1604c1185de960
slashgirl <403741195@qq.com>
2017-06-22 16:01:46 +0800

    test
    阻塞与非阻塞

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,19 @@
+var exec = require("child_process").exec;
+
 function start(){
 	console.log("Request handler 'start' was called.");
-	return "Hello Start";
+	// return "Hello Start";
+	// function sleep(milliSeconds){
+	// 	var startTime = new Date().getTime();
+	// 	while(new Date().getTime() < startTime + milliSeconds);
+	// }
+
+	// sleep(10000);
+	var content = "empty";
+	exec("ls-lah",function(error,stdout,stderr){
+		content = stdout;
+	});
+	return content;
 }
 
 function upload(){
