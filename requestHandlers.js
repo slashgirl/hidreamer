ced2402e71a2216eaa798bbf13d1f0806122bb81
slashgirl <403741195@qq.com>
2017-06-27 14:13:31 +0800

    fix upload function

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var querystring = require("querystring");
+var querystring = require("querystring"),fs = require("fs"),formidable = require("formidable");
 
-function start(response,postData){
+function start(response){
 	console.log("Request handler 'start' was called.");
 
 	var body = '<html>'+
@@ -8,9 +8,9 @@ function start(response,postData){
 				'<meta http-equiv="Content-type" content="text/html; charset=UTF-8"/>'+
 				'</head>'+
 				'<body>'+
-				'<form action="/upload" method="post">'+
-				'<textarea name="text" rows="20" cols="60"></textarea>'+
-				'<input type="submit" value="submit text"/>'+
+				'<form action="/upload" enctype="multipart/form-data" method="post">'+
+				'<input type="file" name="upload" multiple="multiple"/>'+
+				'<input type="submit" value="Upload file"/>'+
 				'</form>'+
 				'</body>'+
 				'</html>';
@@ -20,12 +20,36 @@ function start(response,postData){
 				response.end();
 }
 
-function upload(response,postData){
+function upload(response,request){
 	console.log("Request handler 'upload' was called.");
-	response.writeHead(200,{"Content-type":"text/plain"});
-	response.write("You've sent:" + postData);
-	response.end();
+
+	var form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request,function(error,fields,files){
+		console.log("parsing done");
+		fs.renameSync(files.upload.path,"tmp/test.jpg");
+		response.writeHead(200,{"Content-type":"text/html"});
+		response.write("received image:<br/>");
+		response.write("<img src='/show'>");
+		response.end();
+	});
+}
+
+function show(response){
+	console.log("Request handler 'show' was called.");
+	fs.readFile("tmp/test.jpg","binary",function(error,file){
+		if(error){
+			response.writeHead(500,{"Content-type":"text/plain"});
+			response.write(error + "\n");
+			response.end();
+		}else{
+			response.writeHead(200,{"Content-type":"image/jpg"});
+			response.write(file,"binary");
+			response.end();
+		}
+	});
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
